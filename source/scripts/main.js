dbe50a9cc03c7af9cdf416bb45dcee7d74ff794d
Anton Varnauski <anton.varnauski@gmail.com>
2024-03-12 22:48:07 +0300

    123

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -3,7 +3,6 @@ const mobMenu = document.querySelector('.mobile__nav');
 const menuLinks = document.querySelectorAll('.mobile__nav a');
 const callBtn = document.querySelector('.footer__callback');
 const closeBtn = Array.from(document.querySelectorAll('.modal__close'));
-const videoModal = Array.from(document.querySelectorAll('.production__item--video'));
 
 callBtn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -25,21 +24,5 @@ menuLinks.forEach((link) => {
   link.addEventListener('click', () => {
     menuBtn.classList.remove('nav__burger--open');
     mobMenu.classList.remove('mobile__nav--open');
-  });
-});
-
-videoModal.forEach((video) => {
-  video.querySelectorAll('.production__link').forEach((link) => {
-    link.addEventListener('click', (e) => {
-      e.preventDefault();
-      videoModal.querySelector('.modal').showModal();
-    });
-  });
-});
-
-videoModal.forEach((video) => {
-  video.querySelector('.modal__close').addEventListener('click', (e) => {
-    e.preventDefault();
-    videoModal.querySelector('.modal').close();
   });
 });
